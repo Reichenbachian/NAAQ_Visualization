0c8dbd532517e3f91afd23fc8fc4e0119efa254c
localhost <localhost@localhosts-MacBook-Pro.local>
2017-11-04 15:13:00 -0400

    test

--- RAQ/MainSite/views.py
+++ RAQ/MainSite/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import sys
+sys.path.append('../../raq/src/')
+import manager
+import pdb
 
 # Create your views here.
 def Landing(request):
 	return render(request, 'MainSite/index.html')
+
+
+
+##### REST API
+def get_for_word(request):
+	pdb.set_trace()
+	return manager.get_graph_json(request.GET[0])
